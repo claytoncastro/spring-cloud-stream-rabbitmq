dfab2a02049f5080cb13942ce537d95d83a0bbdc
claytoncastro <claytoncastro.ti@hotmail.com>
2020-06-22 17:36:15 -0300

    Endpoint improvements

--- src/main/java/com/codjavando/project/api/resources/ClientResource.java
+++ src/main/java/com/codjavando/project/api/resources/ClientResource.java
@@ -4,37 +4,30 @@ import com.codjavando.project.api.model.Client;
 import com.codjavando.project.api.services.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/client")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ClientResource {
 
-    private final ClientService clientService;
-
-    @GetMapping
-    public ModelAndView getView() {
-        ModelAndView mv = new ModelAndView("confirmationForm");
-        return mv;
-    }
+    private final ClientService service;
 
     @PostMapping
-    public String save(@Valid Client client, BindingResult result, RedirectAttributes attributes) {
-        if(result.hasErrors()) {
-            attributes.addFlashAttribute("message", "Make sure the required fields are filled in.");
-            return "redirect:/confirmationForm";
-        }
-        clientService.save(client);
-        return "redirect:/confirmation";
+    public ResponseEntity<Void> save(@RequestBody Client client) {
+        service.save(client);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand(client.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
